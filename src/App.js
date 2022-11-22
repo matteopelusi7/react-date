707127f52534b35a4b99fe790e1e24f298c09a91
matteopelusi7 <pelusimatteo02@gmail.com>
2022-11-22 12:28:37 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import List from "./List";
-import {useState} from "react";
 import data from "./data";
+import {useState} from "react";
 
 
 function App() {
@@ -8,34 +8,29 @@ function App() {
   const [people, setPeople] = useState(data);
 
   const removeItem = (id) => {
-    setPeople((oldValue) => oldValue.filter((value) => value.id !== id));
-  }
-
-  const reloadPeople = () => {
-    setPeople(data)
+    setPeople((oldValue) => {
+      return oldValue.filter((value) => value.id !== id);
+    })
   }
 
   const deletePeople = () => {
     setPeople([])
   }
 
+  const reloadPeople = () => {
+    setPeople(data)
+  }
+
   return (
     <section>
       <div className="container">
-        <h2 style={{ color: "var(--bg-blue)" }}>Prossimi Incontri</h2>
+        <h2 style={{color: 'var(--bg-blue)'}}>Prossimi Incontri</h2>
         <div className="people-list">
           <List data={people} removeItem={removeItem}/>
         </div>
         <div className="btn-group">
-          <button className="btn btn-reset" onClick={reloadPeople}>
-            {" "}
-              Reload{" "}
-          </button>
-
-          <button className="btn btn-delete" onClick={deletePeople}>
-            {" "}
-              Delete all
-          </button>
+          <button className="btn btn-reset" onClick={reloadPeople}>Reload</button>
+          <button className="btn btn-delete" onClick={deletePeople}>Delete all</button>
         </div>
       </div>
     </section>
--- src/List.js
+++ src/List.js
@@ -1,33 +1,32 @@
 import React from 'react'
+import {RiDeleteBack2Fill} from 'react-icons/ri'
 
 const List = (props) => {
     return (
-      <>
+      <ul className='user-list'>
         {props.data.map((person) => (
-          <li key={person.id}>
-            {" "}
-            <Person {...person} removeItem={props.removeItem} />
-          </li>
+            <li key={person.id}>
+                <Person {...person} removeItem={props.removeItem}/>
+            </li>
         ))}
-      </>
+      </ul>
     );
 };
   
-const Person = ({ id, nome, stato, img, removeItem }) => {
+const Person = ({id, nome, stato, img, removeItem}) => {
     return (
         <article>
-        <img src={img} alt="prs" className="person-img" />
-        <div className="person-info">
-          <div className="person-action">
-            <h4>{nome}</h4>
-            <button className="btn" onClick={() => removeItem(id)}>
-              {" "}
-               X
-            </button>
-          </div>
-          <p>{stato}</p>
-        </div>
-      </article>
+            <img src={img} alt="prs" className='person-img'/>
+            <div className='person-info'>
+                <div className='person-action'>
+                    <h4>{nome}</h4>
+                    <button className='btn' onClick={() => removeItem(id)}>
+                        <RiDeleteBack2Fill className='icon' />
+                    </button>
+                </div>
+                <p>{stato}</p>
+            </div>
+        </article>
     );
 };
 
