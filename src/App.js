94e229d498515b4a99e4441384f5385a4910f847
matteopelusi7 <pelusimatteo02@gmail.com>
2022-11-22 11:38:17 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import List from "./List";
+import {useState} from "react";
+import data from "./data";
+
 
 function App() {
+
+  const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    setPeople((oldValue) => oldValue.filter((value) => value.id !== id));
+  }
+
+  const reloadPeople = () => {
+    setPeople(data)
+  }
+
+  const deletePeople = () => {
+    setPeople([])
+  }
+
   return (
-    <div className="App">
-      <List />
-    </div>
+    <section>
+      <div className="container">
+        <h2 style={{ color: "var(--bg-blue)" }}>Prossimi Incontri</h2>
+        <div className="people-list">
+          <List data={people} removeItem={removeItem}/>
+        </div>
+        <div className="btn-group">
+          <button className="btn btn-reset" onClick={reloadPeople}>
+            {" "}
+              Reload{" "}
+          </button>
+
+          <button className="btn btn-delete" onClick={deletePeople}>
+            {" "}
+              Delete all
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- src/List.js
+++ src/List.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
-const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+const List = (props) => {
+    return (
+      <>
+        {props.data.map((person) => (
+          <li key={person.id}>
+            {" "}
+            <Person {...person} removeItem={props.removeItem} />
+          </li>
+        ))}
+      </>
+    );
+};
+  
+const Person = ({ id, nome, stato, img, removeItem }) => {
+    return (
+        <article>
+        <img src={img} alt="prs" className="person-img" />
+        <div className="person-info">
+          <div className="person-action">
+            <h4>{nome}</h4>
+            <button className="btn" onClick={() => removeItem(id)}>
+              {" "}
+               X
+            </button>
+          </div>
+          <p>{stato}</p>
+        </div>
+      </article>
+    );
+};
 
 export default List
